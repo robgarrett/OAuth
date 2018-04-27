d725ac4c3fe6695fd68cf152338ae508f23e788c
Rob Garrett <rob@robgarrett.com>
2018-04-27 15:23:03 -0400

    Initial commit

--- Demo-MVC-OAuth/Controllers/HomeController.cs
+++ Demo-MVC-OAuth/Controllers/HomeController.cs
@@ -1,7 +1,5 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
+using System.Configuration;
 using System.Web.Mvc;
 
 namespace Demo_MVC_OAuth.Controllers
@@ -10,21 +8,47 @@ namespace Demo_MVC_OAuth.Controllers
     {
         public ActionResult Index()
         {
+            var code = Request.QueryString["code"];
+            var state = Request.QueryString["state"];
+            if (string.IsNullOrEmpty(code)) return View();
+            if (string.IsNullOrEmpty(state)) return View("Error");
+            // Exchange the code for an access token.
+            state = state.ToLower();
+            if (state == "github")
+            {
+                var provider = new OAuth2.OAuth2Provider()
+                {
+                    ClientId = ConfigurationManager.AppSettings["ClientId"],
+                    ClientSecret = ConfigurationManager.AppSettings["ClientSecret"],
+                    AccessTokenUri = new Uri("https://github.com/login/oauth/access_token"),
+                    UserInfoUri = new Uri("https://api.github.com/user"),
+                    State = "GitHub"
+                };
+                var response = OAuth2.AuthenticateByCode(provider, Request.UrlHome(), code);
+                if (null == response) return View("Error");
+                // Use the API here...
+            }
             return View();
         }
 
-        public ActionResult About()
+        [HttpPost]
+        public ActionResult ProcessForm(string submit)
         {
-            ViewBag.Message = "Your application description page.";
-
-            return View();
-        }
-
-        public ActionResult Contact()
-        {
-            ViewBag.Message = "Your contact page.";
-
-            return View();
+            if (string.IsNullOrEmpty(submit)) return View("Index");
+            submit = submit.ToLower();
+            if (submit == "github")
+            {
+                var provider = new OAuth2.OAuth2Provider()
+                {
+                    ClientId = ConfigurationManager.AppSettings["ClientId"],
+                    ClientSecret = ConfigurationManager.AppSettings["ClientSecret"],
+                    AuthUri = new Uri("https://github.com/login/oauth/authorize"),
+                    State = "GitHub"
+                };
+                var url = OAuth2.CreateRedirect(provider, Request.UrlHome());
+                return Redirect(url.ToString());
+            }
+            return View("Index");
         }
     }
 }
---
+++ Demo-MVC-OAuth/Helper.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Web;
+using JetBrains.Annotations;
+
+namespace Demo_MVC_OAuth
+{
+    internal static class Helper
+    {
+        [PublicAPI]
+        public static Uri UrlOriginal(this HttpRequestBase request)
+        {
+            var hostHeader = request.Headers["host"];
+            return request.Url != null ? new Uri($"{request.Url.Scheme}://{hostHeader}{request.RawUrl}") : null;
+        }
+
+        [PublicAPI]
+        public static Uri UrlHome(this HttpRequestBase request)
+        {
+            var hostHeader = request.Headers["host"];
+            return request.Url != null ? new Uri($"{request.Url.Scheme}://{hostHeader}/Home/Index") : null;
+        }
+    }
+}
