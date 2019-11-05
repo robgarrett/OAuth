e884b69fe8229fadd944c92f048587dd36e3fce9
Rob Garrett <rob@robgarrett.com>
2019-11-05 18:36:07 -0500

    Removed sensitive data

---
+++ SP-Core/spoauth/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace spoauth
+{
+    /*
+        Ported from https://github.com/s-KaiNet/node-sp-auth.
+     */
+    class Program
+    {
+        class Creds : IUserCredentials
+        {
+            public string username { get; set; }
+            public string password { get; set; }
+            public bool? online { get; set; }
+
+            public Creds(string username, string password, bool? online = null)
+            {
+                this.username = username;
+                this.password = password;
+                this.online = online;
+            }
+
+            public IUserCredentials Clone(IUserCredentials creds)
+            {
+                return new Creds(username, password, online);
+            }
+        }
+        static async Task Main(string[] args)
+        {
+            var resolver = AuthResolverFactory.resolve("https://rdgdemo.sharepoint.com", new Creds("admin@rdgdemo.onmicrosoft.com", "password"));
+            await resolver.getAuthAsync();
+        }
+    }
+}
