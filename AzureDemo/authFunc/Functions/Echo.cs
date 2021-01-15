5a8e1f5c1965043b7288376352368019ea9c21ac
Rob Garrett <rob@robgarrett.com>
2021-01-15 18:24:43 -0500

    Skeleton Azure Function

---
+++ AzureDemo/authFunc/Functions/Echo.cs
@@ -0,0 +1,23 @@
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Azure.WebJobs.Extensions.Http;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Logging;
+
+namespace authFunc
+{
+    public class EchoFunc
+    {
+        [FunctionName("Echo")]
+        public async Task<IActionResult> Echo([HttpTrigger(AuthorizationLevel.Anonymous, "get", Route = "echo")] HttpRequest req, ILogger log)
+        {
+            return await Task<IActionResult>.Run(() =>
+            {
+                var msg = req.Query["msg"];
+                var result = (!string.IsNullOrEmpty(msg)) ? $"Hello {msg}" : "Hello";
+                return new OkObjectResult(result);
+            });
+        }
+    }
+}
---
+++ AzureDemo/authFunc/gulpfile.babel.js
@@ -0,0 +1,59 @@
+import gulp from "gulp";
+import { spawn } from "child_process";
+
+let { clean, build, restore, publish } = require("gulp-dotnet-cli");
+
+// ** Clean ** /
+gulp.task("clean", () => {
+    return gulp.src("**/*.csproj", { read: false }).pipe(clean());
+});
+
+// ** Build ** /
+gulp.task("prebuild", (done) => {
+    return done();
+});
+
+gulp.task("postbuild", (done) => {
+    return done();
+});
+
+gulp.task("restore", () => {
+    return gulp.src("**/*.csproj", { read: false }).pipe(restore());
+});
+
+gulp.task("compile", () => {
+    return gulp.src("**/*.csproj", { read: false }).pipe(build());
+});
+
+gulp.task("dockerize", (done) => {
+    var cmd = spawn("docker-compose", ["build", "auth-func-dev"], { stdio: 'inherit' });
+    cmd.on("close", (code) => {
+        done(code);
+    });
+});
+
+gulp.task("build", gulp.series("prebuild", "restore", "compile", "postbuild"));
+
+gulp.task("build:full", gulp.series("prebuild", "restore", "compile", "postbuild", "dockerize"));
+
+// ** Publish ** /
+gulp.task("publish:build", () => {
+    return gulp.src("**/*.csproj", { read: false }).pipe(publish({ configuration: "Release" }));
+});
+gulp.task("postpublish", (done) => {
+    return done();
+});
+gulp.task("publish", gulp.series("publish:build", "postpublish"));
+
+// ** Run ** /
+gulp.task("run-build", (done) => {
+    var cmd = spawn("docker-compose", ["run", "--service-ports", "auth-func-dev"], { stdio: 'inherit' });
+    cmd.on("close", (code) => {
+        done(code);
+    });
+});
+
+gulp.task("run", gulp.series("build:full", "run-build"));
+
+// ** Default ** /
+gulp.task("default", gulp.series("run"));
